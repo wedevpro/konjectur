0978d21e91a535753b3ee774deb8d383f2e35b5e
Antoine GARIN <antoine.garin@gmail.com>
2026-05-06 16:50:56 +0200

    Reactivate particles

--- src/js/script.js
+++ src/js/script.js
@@ -44,37 +44,37 @@ function loadLang(){
 }
 
 // PARTICLES
-// const canvas = document.getElementById('bg');
-// const ctx = canvas.getContext('2d');
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
+const canvas = document.getElementById('bg');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// let particles = [];
-// for(let i=0;i<80;i++){
-//   particles.push({
-//     x:Math.random()*canvas.width,
-//     y:Math.random()*canvas.height,
-//     vx:(Math.random()-0.5)*1,
-//     vy:(Math.random()-0.5)*1
-//   });
-// }
+let particles = [];
+for(let i=0;i<80;i++){
+  particles.push({
+    x:Math.random()*canvas.width,
+    y:Math.random()*canvas.height,
+    vx:(Math.random()-0.5)*1,
+    vy:(Math.random()-0.5)*1
+  });
+}
 
-// function animate(){
-//   ctx.clearRect(0,0,canvas.width,canvas.height);
-//   particles.forEach(p=>{
-//     p.x+=p.vx;
-//     p.y+=p.vy;
+function animate(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  particles.forEach(p=>{
+    p.x+=p.vx;
+    p.y+=p.vy;
 
-//     if(p.x<0||p.x>canvas.width) p.vx*=-1;
-//     if(p.y<0||p.y>canvas.height) p.vy*=-1;
-//     ctx.beginPath();
-//     ctx.arc(p.x,p.y,2,0,Math.PI*2);
-//     ctx.fillStyle='#41d0d4';
-//     ctx.fill();
-//   });
-//   requestAnimationFrame(animate);
-// }
-// animate();
+    if(p.x<0||p.x>canvas.width) p.vx*=-1;
+    if(p.y<0||p.y>canvas.height) p.vy*=-1;
+    ctx.beginPath();
+    ctx.arc(p.x,p.y,2,0,Math.PI*2);
+    ctx.fillStyle='#41d0d4';
+    ctx.fill();
+  });
+  requestAnimationFrame(animate);
+}
+animate();
 
 // SCROLL ANIMATION
 const observer = new IntersectionObserver(entries=>{
