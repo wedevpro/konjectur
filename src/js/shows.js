11de018fd7309f8262e4eff0421fc5af357eb267
Antoine GARIN <antoine.garin@gmail.com>
2026-05-12 00:28:40 +0200

    ...

--- src/js/shows.js
+++ src/js/shows.js
@@ -3,13 +3,13 @@ const shows = [
         date: "2026-05-08T21:00",
         place: "Hirson (02) - L'Île Verte",
         link: "https://www.facebook.com/events/1499235331693953",
-        image: "assets/shows/2026-05-08.jpg"
+        images: ["assets/shows/2026-05-08_hirson/1.jpg", "assets/shows/2026-05-08_hirson/2.jpg", "assets/shows/2026-05-08_hirson/3.png", "assets/shows/2026-05-08_hirson/4.jpg", "assets/shows/2026-05-08_hirson/5.jpg", "assets/shows/2026-05-08_hirson/6.jpg", "assets/shows/2026-05-08_hirson/7.jpg", "assets/shows/2026-05-08_hirson/8.jpg", "assets/shows/2026-05-08_hirson/9.jpg", "assets/shows/2026-05-08_hirson/10.jpg", "assets/shows/2026-05-08_hirson/11.jpg", "assets/shows/2026-05-08_hirson/12.jpg", "assets/shows/2026-05-08_hirson/13.jpg"]
     },
     {
         date: "2030-01-10T20:30",
         place: "Saint-Denis - Stade de France",
         link: "https://www.facebook.com/events/1499235331693953",
-        image: "assets/shows/2026-05-08.jpg"
+        images: ["assets/shows/2026-05-08.jpg"]
     }
 ];
 shows.sort((a, b) => new Date(a.date) - new Date(b.date));
@@ -37,17 +37,23 @@ function renderShows() {
         <div class="show-actions">
           <a href="${show.link}" target="_blank" class="btn-glow event-btn" data-i18n="shows_event">
           </a>
-
-          ${show.image ? `
-            <button class="btn-glow flyer-btn" onclick="toggleImage(${index})" data-i18n="shows_flyer" data-index="${index}">
+            ${show.images?.length ? `
+            <button class="btn-glow photos-btn"
+                    data-index="${index}"
+                    data-i18n="shows_photos"
+                    onclick="toggleGallery(${index})">
             </button>
-          ` : ''}
+            ` : ''}
         </div>
       </div>
 
-      <div class="show-img" id="show-img-${index}">
-        <img src="${show.image}" loading="lazy">
-      </div>
+     <div class="show-gallery" id="show-gallery-${index}">
+
+  ${show.images.map(img => `
+    <img src="${img}" loading="lazy" onclick="openLightbox(this.src)" />
+  `).join('')}
+
+</div>
     `;
 
         container.appendChild(el);
@@ -61,19 +67,47 @@ function formatShowDate(date) {
     });
 }
 
-function toggleImage(i) {
-    const img = document.getElementById('show-img-' + i);
-    const btn = document.querySelector(`.flyer-btn[data-index="${i}"]`);
+function toggleGallery(i){
 
-    const isVisible = img.style.display === 'block';
+  const gallery =
+    document.getElementById(`show-gallery-${i}`);
 
-    img.style.display = isVisible ? 'none' : 'block';
+  const btn =
+    document.querySelector(
+      `.photos-btn[data-index="${i}"]`
+    );
 
-    if (btn) {
-        btn.classList.toggle('active', !isVisible);
-        btn.blur();
-    }
+  const isVisible =
+    gallery.style.display === 'grid';
+
+  gallery.style.display =
+    isVisible ? 'none' : 'grid';
+
+  btn.classList.toggle('active', !isVisible);
+
+  btn.blur();
 }
+
+const lightbox =
+  document.getElementById('lightbox');
+
+const lightboxImg =
+  document.getElementById('lightbox-img');
+
+function openLightbox(src){
+
+console.log('opening lightbox with src:', src);
+  lightboxImg.src = src;
+
+  lightbox.classList.add('open');
+  document.body.classList.add('lightbox-open');
+}
+
+lightbox.addEventListener('click', () => {
+
+  lightbox.classList.remove('open');
+  document.body.classList.remove('lightbox-open');
+});
 
 renderShows();
 
