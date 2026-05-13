3d7ba304b1f3b4e13005eb14e09270afed45227d
Antoine GARIN <antoine.garin@gmail.com>
2026-05-13 10:46:09 +0200

    Page de maintenance

--- deploy.js
+++ deploy.js
@@ -1,41 +1,95 @@
 require('dotenv').config();
-const FtpDeploy = require("ftp-deploy");
+
+const path = require('path');
+const FtpDeploy = require('ftp-deploy');
+
 const ftpDeploy = new FtpDeploy();
 
-const config = {
+const baseConfig = {
+
   user: process.env.FTP_USER,
-  // Password optional, prompted if none given
   password: process.env.FTP_PASS,
   host: process.env.FTP_HOST,
+
   port: 21,
-  localRoot: __dirname + "/dist",
-  remoteRoot: "/www/",
-  include: ["*", ".htaccess"],      // this would upload everything except dot files
-  // include: ["*.php", "dist/*", ".*"],
-  // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
-  // exclude: [
-  //     "dist/**/*.map",
-  //     "node_modules/**",
-  //     "node_modules/**/.*",
-  //     ".git/**",
-  // ],
-  // delete ALL existing files at destination before uploading, if true
-  deleteRemote: true,
-  // Passive mode is forced (EPSV command is not sent)
+
+  remoteRoot: '/www/',
+
   forcePasv: true,
-  // use sftp or ftp
+
   sftp: false,
+
 };
 
-console.log(`🚀 Envoi des fichiers depuis ${config.localRoot.replace('/', '\\')}`);
+async function deploy(){
 
-ftpDeploy
-  .deploy(config)
-  .then((res) => {
-    console.log(res);
-    console.log("✅ Déploiement terminé !");
-  })
-  .catch((err) => {
+  try {
+
+    console.log('🟡 Activation maintenance...');
+
+    // 1. Upload maintenance.html -> index.html
+    await ftpDeploy.deploy({
+
+      ...baseConfig,
+
+      localRoot: path.join(__dirname, 'maintenance'),
+
+      include: ['index.html'],
+
+      deleteRemote: false,
+
+    });
+
+    console.log('✅ Maintenance activée');
+
+
+
+    console.log('🚀 Upload du site...');
+
+    // 2. Upload du site complet
+    await ftpDeploy.deploy({
+
+      ...baseConfig,
+
+      localRoot: path.join(__dirname, 'dist'),
+      include: ["*", ".htaccess"], 
+      exclude: ['index.html'],
+
+      deleteRemote: false,
+
+    });
+
+    console.log('✅ Fichiers uploadés');
+
+
+
+    console.log('🟢 Publication du site...');
+
+    // 3. Upload du vrai index.html
+    await ftpDeploy.deploy({
+
+      ...baseConfig,
+
+      localRoot: path.join(__dirname, 'dist'),
+
+      include: ['index.html'],
+
+      deleteRemote: false,
+
+    });
+
+    console.log('🎉 Déploiement terminé');
+
+  }
+
+  catch(err){
+
     console.error(err);
-    console.log("❌ Erreur de déploiement");
-  });
+
+    console.log('❌ Erreur déploiement');
+
+  }
+
+}
+
+deploy();
